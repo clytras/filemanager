c9febd8f461d13ae591a7cd4aac12ea2f2ee724b
Igors Krasjukovs <tahq69@gmail.com>
2016-03-01 13:22:30 +0200

    icon class refactored for new mime class;

--- src/Services/Icon.php
+++ src/Services/Icon.php
@@ -14,11 +14,6 @@ class Icon
     public $path;
 
     /**
-     * @var bool
-     */
-    private $is_changed = false;
-
-    /**
      * @var array
      */
     protected $icon_names = [
@@ -37,18 +32,10 @@ class Icon
         'excel' => 'excel.png',
     ];
 
-    /**
-     * Sets path for icons
-     *
-     * @param $path
-     *
-     * @return $this
-     */
-    public function setPath($path)
+    public function __construct()
     {
-        $this->path = $path;
-
-        return $this;
+        $this->icon_names = array_merge($this->icon_names, FileManager::package()->config('icons.files', []));
+        $this->path = FileManager::package()->config('icons.path', '');
     }
 
     /**
@@ -58,61 +45,7 @@ class Icon
      */
     public function get(Mime $file_mime_type)
     {
-        $this->change();
-
-        switch ($file_mime_type->fileType()) {
-            case 'dir':
-                return $this->result('dir');
-            case 'image':
-                return $this->result('img');
-        }
-
-        if ($file_mime_type->isVideo()) {
-            return $this->result('video');
-        }
-
-        if ($file_mime_type->isAudio()) {
-            return $this->result('audio');
-        }
-
-        switch ($file_mime_type->__toString()) {
-            case 'application/javascript':
-            case 'application/json':
-            case 'application/x-javascript':
-            case 'text/javascript':
-            case 'text/x-jquery-tmpl':
-                return $this->result('js');
-
-            case 'application/x-gzip':
-            case 'application/x-rar-compressed':
-            case 'application/zip':
-                return $this->result('zip');
-
-            case 'text/css':
-                return $this->result('css');
-
-            case 'text/plain':
-                return $this->result('txt');
-
-            case 'application/xhtml+xml':
-            case 'text/html':
-                return $this->result('html');
-
-            case 'application/msword':
-            case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-                return $this->result('word');
-
-            case 'application/vnd.ms-excel':
-            case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-                return $this->result('excel');
-
-            case 'application/vnd.ms-powerpoint':
-            case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
-                return $this->result('pwp');
-
-            default:
-                return $this->result('any');
-        }
+        return $this->result($file_mime_type->fileType());
     }
 
     /**
@@ -122,21 +55,5 @@ class Icon
     private function result($key)
     {
         return $this->path . $this->icon_names[$key];
-    }
-
-    /**
-     * Update icon names from config, if they are not yet updated
-     * Set path from config if it is empty
-     */
-    private function change()
-    {
-        if (!$this->is_changed) {
-            $this->icon_names = array_merge($this->icon_names, FileManager::package()->config('icons.files', []));
-            $this->is_changed = true;
-        }
-
-        if (is_null($this->path)) {
-            $this->setPath(FileManager::package()->config('icons.path', ''));
-        }
     }
 }
