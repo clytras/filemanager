bf4dbb15c9d2e164baa43270b01ee21e7d29f1c1
Igors Krasjukovs <tahq69@gmail.com>
2016-04-24 20:26:50 +0300

    item properties modal fixed for new translator;

--- src/resources/assets/js/file-manager/controllers/ItemPropertiesController.js
+++ src/resources/assets/js/file-manager/controllers/ItemPropertiesController.js
@@ -8,10 +8,17 @@
     ];
 
     function ItemPropertiesController($log, $scope, $mdDialog, Trans, item) {
-        activate();
 
-        function activate() {
-            $log.info(item);
+        // when translations is loaded, begin initialize page content
+        Trans.then(activate);
+
+        var trans = {};
+
+        function activate(translations) {
+            // add translations for global variable, to be available in all methods
+            trans = translations;
+
+            //$log.info(item);
             $scope.item = resolveItemDetails(item);
             $scope.thumb = item.thumb;
             $scope.name = item.full_name;
@@ -50,18 +57,18 @@
          * @param {function} item.getSize
          */
         function defaultDetails(details, item) {
-            $log.log('item_properties_modal_file_type_' + item.type);
+            //$log.log('item_properties_modal_file_type_' + item.type);
             details.push({
-                name: Trans('item_properties_modal_item_type'),
-                value: Trans('item_properties_modal_file_type_' + item.type)
+                name: trans['item_properties_modal_item_type'],
+                value: trans['item_properties_modal_file_type_' + item.type]
             }, {
-                name: Trans('item_properties_modal_name'),
+                name: trans['item_properties_modal_name'],
                 value: item.full_name
             }, {
-                name: Trans('item_properties_modal_date'),
+                name: trans['item_properties_modal_date'],
                 value: item.updated_at
             }, {
-                name: Trans('item_properties_modal_size'),
+                name: trans['item_properties_modal_size'],
                 value: item.getSize()
             });
         }
@@ -91,40 +98,40 @@
 
             if (item.dir !== '') {
                 details.push({
-                    name: Trans('item_properties_modal_item_dir'),
+                    name: trans['item_properties_modal_item_dir'],
                     value: item.dir
                 });
             }
 
             details.push({
-                name: Trans('item_properties_modal_item_extension'),
+                name: trans['item_properties_modal_item_extension'],
                 value: item.extension
             });
 
             if (item.type === 'image' && ng.hasValue(item.thumbs)) {
 
                 details.push({
-                    name: Trans('item_properties_modal_item_url'),
+                    name: trans['item_properties_modal_item_url'],
                     value: '<a href="{url}" target="_blank">{title}</a>'.supplant({
                         url: item.url,
-                        title: Trans('item_properties_modal_size_dim').supplant(item.size)
+                        title: trans['item_properties_modal_size_dim'].supplant(item.size)
                     })
                 });
 
                 ng.forEach(item.thumbs, function (val, size) {
                     details.push({
-                        name: Trans('item_properties_modal_size_url_title').supplant({
-                            size: Trans('item_properties_modal_size_key_' + size)
+                        name: trans['item_properties_modal_size_url_title'].supplant({
+                            size: trans['item_properties_modal_size_key_' + size]
                         }),
                         value: '<a href="{url}" target="_blank">{title}</a>'.supplant({
                             url: val.url,
-                            title: Trans('item_properties_modal_size_dim').supplant(val.size)
+                            title: trans['item_properties_modal_size_dim'].supplant(val.size)
                         })
                     });
                 });
             } else {
                 details.push({
-                    name: Trans('item_properties_modal_item_url'),
+                    name: trans['item_properties_modal_item_url'],
                     value: '<a href="{url}" target="_blank">{full_name}</a>'.supplant(item)
                 });
             }
